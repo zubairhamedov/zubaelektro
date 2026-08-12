ce4d12c038965c676553b02af5063079249e9a42
ZUBAIR HAMEDOV <zubairxamedov15@icloud.com>
2026-08-12 14:33:42 +0600

    Create types.ts

---
+++ lib/simulator/types.ts
@@ -0,0 +1,51 @@
+export type WireColor = "faza" | "nol" | "yer";
+
+export type PortType = "L" | "N" | "PE" | "generic";
+
+export type PortDef = {
+  id: string;
+  label: string;
+  type: PortType;
+  x: number;
+  y: number;
+};
+
+export type ElementType =
+  | "manba"
+  | "lampa"
+  | "kalit"
+  | "otish_kalit"
+  | "rozetka"
+  | "korobka"
+  | "avtomat"
+  | "uzo";
+
+export type ElementDef = {
+  type: ElementType;
+  label: string;
+  width: number;
+  height: number;
+  ports: PortDef[];
+  internalLinks: [string, string][];
+};
+
+export type PlacedElement = {
+  id: string;
+  type: ElementType;
+  x: number;
+  y: number;
+};
+
+export type Wire = {
+  id: string;
+  fromElementId: string;
+  fromPort: string;
+  toElementId: string;
+  toPort: string;
+  color: WireColor;
+};
+
+export type CanvasState = {
+  elements: PlacedElement[];
+  wires: Wire[];
+};
